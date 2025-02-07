b6a2fecf358dc53e383cc2ce0edd8146c2b1c149
Yusei <yuseishimizu02020825@gmail.com>
2025-02-07 17:36:18 +0900

    tinko

--- imports.js
+++ imports.js
@@ -1,10 +1,10 @@
 const sub_question_list = {
     rika_ue:["a","a","a","a","a","a","a","a","a","a","a","a","a","a",],
-    rika_ima:[],
-    rika_shuri:[],
-    rika_oti:[],
-    rekisi:[],
-    komin:[],
-    kobun:[]
+    rika_ima:["i","i"],
+    rika_shuri:["u","u"],
+    rika_oti:["e","e"],
+    rekishi:["o","o"],
+    komin:["k","k"],
+    kobun:["s","s"]
 }
 const subject_list = ["rika_ue","rika_ima","rika_shuri","rika_oti","rekishi","komin","kobun"]
--- main.js
+++ main.js
@@ -1,17 +1,15 @@
 let question_element = document.getElementById("question")
+let questions_element = document.getElementById("questions")
 let answer_element = document.getElementById("answer")
-let uncorrect_questoins_element = document.getElementById("kaitou")
-let subject_option = document.getElementById("subjects")
-let options = document.querySelectorAll("#subjects option");
+let not_right = document.getElementById("kaitou")
+let subjects = document.getElementById("subjects")
+let sub_options = document.querySelectorAll("#subjects option")
+let time_chage = document.getElementById("time_change")
+let time_options = document.querySelectorAll("#time_change option")
+let time_limit = document.getElementById("time_limit")
+
 answer_element.addEventListener("change",changeFunc);
-subject_option.addEventListener("change" , () => {
-    index = subject_option.selectedIndex
-    if (subject_option.selectedIndex==0){return};
-    question_list = sub_question_list[options[index].id]
-    console.log(question_list,options[index].id)
-    not_question_num = [...Array(question_list.length/2).keys()].map((d) => {return d})
-    random_question();
-});
+
 
 const maru = new Audio('maru.mp3')
 const batsu = new Audio('batsu.mp3')
@@ -21,7 +19,46 @@ let q_number;
 let question_list;
 let not_question_num;
 let index;
+let qs_state = false
+let time_limit_num = 5000
 
+time_chage.addEventListener("change" , () =>{
+    time_limit_num=Number(time_options[time_chage.selectedIndex].id)*1000
+})
+
+subjects.addEventListener("change" , () =>{
+    index = subjects.selectedIndex
+    while (not_right.firstChild){not_right.removeChild(not_right.firstChild)};
+    if (subjects.selectedIndex==0){
+        qs_state = false
+        console.log("off");
+        return;
+    }else {
+        if (!qs_state){questions_element.classList.toggle("display_change")};
+        qs_state = true
+        console.log("on");
+    };
+    question_list = sub_question_list[sub_options[index].id]
+    console.log(question_list,sub_options[index].id)
+    not_question_num = [...Array(question_list.length/2).keys()].map((d) => {return d})
+    random_question();
+});
+
+function Counter() {
+    this.value;
+}
+let n;
+Counter.prototype.start = () =>{n = setInterval(() =>{
+    if (counter.value==0) {
+        ;
+    }else{
+        time_limit.textContent = `${counter.value-1}秒`;
+        console.log("titin")
+        counter.value-=1;
+    };
+} , 1000)}
+Counter.prototype.finish = () =>{clearInterval(n)}
+const counter = new Counter()
 
 function changeFunc(e){
     ans = e.target.value
@@ -42,14 +79,17 @@ function changeFunc(e){
 
 function random_question(){
     if (not_question_num.length>0){
+        time_limit.textContent =  `${time_limit_num/1000}秒`
+        counter.value = time_limit_num/1000
         rand = Math.floor(Math.random() * (not_question_num.length))
         console.log(not_question_num.length)
         qnum = not_question_num[rand]
         question_element.innerText = question_list[qnum*2]
         q_number = qnum;
         console.log(rand,qnum,not_question_num)
+        counter.start()
     
-        setTimeout(q_time_out,8000,qnum);
+        setTimeout(q_time_out,time_limit_num,qnum);
     
         not_question_num.splice(rand,1)
     }
@@ -57,12 +97,13 @@ function random_question(){
 
 function q_time_out(q_num){
     if (q_num==q_number){
+        counter.finish()
         maru.pause()
         maru.currentTime = 0
         batsu.pause()
         batsu.currentTime = 0
         batsu.play()
-        uncorrect_questoins_element.innerHTML += question_list[qnum*2] + " : " + question_list[qnum*2+1] + "<br><br>"
+        not_right.innerHTML += "<br><br>" + question_list[qnum*2] + " : " + question_list[qnum*2+1] 
         random_question()
     }
 }
