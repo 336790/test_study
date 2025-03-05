46ec7e22cab1402a64e7750731e62bcf59ebcf31
Yusei <yuseishimizu02020825@gmail.com>
2025-03-06 03:47:59 +0900

    いろいろ

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//html要素を取得
 let question_element = document.getElementById("question")
 let questions_element = document.getElementById("questions")
 let answer_element = document.getElementById("answer")
@@ -7,13 +8,14 @@ let sub_options = document.querySelectorAll("#subjects option")
 let time_chage = document.getElementById("time_change")
 let time_options = document.querySelectorAll("#time_change option")
 let time_limit = document.getElementById("time_limit")
+let select_q_type = document.getElementById("q_type")
+let display_q_number = document.getElementById("q_number")
 
-answer_element.addEventListener("change",changeFunc);
 
 
+//いろいろ定義
 const maru = new Audio('maru.mp3')
 const batsu = new Audio('batsu.mp3')
-
 
 let q_number;
 let question_list;
@@ -21,30 +23,16 @@ let not_question_num;
 let index;
 let qs_state = false
 let time_limit_num = 15000
+let q_type_state = false
+let displayed_number = 1;
+let result = {
+    total:0,
+    right:1,
+    wrong:0
+}
+let finished =false;
 
-time_chage.addEventListener("change" , () =>{
-    time_limit_num=Number(time_options[time_chage.selectedIndex].id)*1000
-})
-
-subjects.addEventListener("change" , () =>{
-    index = subjects.selectedIndex
-    counter.finish()
-    while (not_right.firstChild){not_right.removeChild(not_right.firstChild)};
-    if (subjects.selectedIndex==0){
-        qs_state = false
-        console.log("off");
-        return;
-    }else {
-        if (!qs_state){questions_element.classList.toggle("display_change")};
-        qs_state = true
-        console.log("on");
-    };
-    question_list = sub_question_list[sub_options[index].id]
-    console.log(question_list,sub_options[index].id)
-    not_question_num = [...Array(question_list.length/2).keys()].map((d) => {return d})
-    random_question();
-});
-
+//Counterクラス生成
 function Counter() {
     this.value;
 }
@@ -59,8 +47,25 @@ Counter.prototype.start = () =>{n = setInterval(() =>{
     };
 } , 1000)}
 Counter.prototype.finish = () =>{clearInterval(n);console.log("clear")}
+//インスタンス化
 const counter = new Counter()
 
+
+
+//イベントリスナーたち
+answer_element.addEventListener("change" , changeFunc);
+
+time_chage.addEventListener("change" , () =>{
+    time_limit_num=Number(time_options[time_chage.selectedIndex].id)*1000
+})
+
+subjects.addEventListener("change" , sub_change);
+
+select_q_type.addEventListener("click" , () => {q_type_state=!q_type_state});
+
+
+
+//メインの関数
 function changeFunc(e){
     ans = e.target.value
     console.log("ans : "+ans)
@@ -75,6 +80,7 @@ function changeFunc(e){
         answer_element.value = ""
         counter.finish()
         random_question()
+        result.right++
     }else if(ans == "しね"){
         q_time_out(q_number)
     }else{
@@ -86,17 +92,27 @@ function random_question(){
     if (not_question_num.length>0){
         time_limit.textContent =  `${time_limit_num/1000}秒`
         counter.value = time_limit_num/1000
-        rand = Math.floor(Math.random() * (not_question_num.length))
+        rand = q_type_state ?
+             Math.floor(Math.random() * (not_question_num.length))
+            :0
         console.log(not_question_num.length)
         qnum = not_question_num[rand]
         question_element.innerText = question_list[qnum*2]
+        display_q_number.innerText = `第${displayed_number}問`
         q_number = qnum;
         console.log(rand,qnum,not_question_num)
+        displayed_number++
         counter.start()
-    
+
         setTimeout(q_time_out,time_limit_num,qnum);
-    
+
         not_question_num.splice(rand,1)
+    }else{
+        if(!finished){
+            finished = true
+            display_q_number.innerText = "終了"
+            question_element.innerText = `${result.total}問中 正解${result.right}問,不正解${result.wrong}...正答率${Math.floor(result.right/result.total*100)}%`
+        }
     }
 }
 
@@ -110,9 +126,37 @@ function q_time_out(q_num){
         batsu.play()
         answer_element.value=""
         const p = document.createElement("p");
-        p.innerText = String(question_list[qnum*2] + " : " + question_list[qnum*2+1] )
+        p.innerText = `第${String(displayed_number-1)}問 : ${String(question_list[q_num*2])}  答 ${String(question_list[qnum*2+1] )}`
         p.classList.add("add")
         not_right.prepend(p)
+        result.wrong++
         random_question()
     }
 }
+
+function sub_change(){
+    index = subjects.selectedIndex
+    displayed_number = 1
+    finished = false
+    counter.finish()
+    while (not_right.firstChild){not_right.removeChild(not_right.firstChild)};
+    if (subjects.selectedIndex==0){
+        console.log(index)
+        qs_state = false
+        console.log("off");
+        return;
+    }else {
+        if (!qs_state){questions_element.classList.toggle("display_change")};
+        qs_state = true
+        console.log("on");
+    };
+    question_list = sub_question_list[sub_options[index].id]
+    result = {
+        total:question_list.length/2,
+        right:0,
+        wrong:0
+    }
+    console.log(question_list,sub_options[index].id)
+    not_question_num = [...Array(question_list.length/2).keys()].map((d) => {return d})
+    random_question();
+};
